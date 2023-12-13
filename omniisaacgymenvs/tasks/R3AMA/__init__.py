89b93944101b5b6bf966f513ce839c3f9d1c55ba
JunghwanRo <jhro@seas.upenn.edu>
2023-12-13 09:54:00 +0100

    Added R3AMA folder to task

---
+++ omniisaacgymenvs/tasks/R3AMA/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "Antoine Richard, Junghwan Ro, Matteo El Hariry"
+__copyright__ = (
+    "Copyright 2023, Space Robotics Lab, SnT, University of Luxembourg, SpaceR"
+)
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Junghwan Ro"
+__email__ = "jro37@gatech.edu"
+__status__ = "development"
